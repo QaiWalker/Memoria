a891bc8aaeb6e5b1487a92e81a2527c42c6d4215
QaiWalker <16014753@myrp.edu.sg>
2018-07-22 14:01:19 +0800

    Implemented SplashScreen

--- app/src/main/java/sg/edu/rp/c346/memoria/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/memoria/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -45,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         tv_p1 = (TextView)findViewById(R.id.tv_p1);
